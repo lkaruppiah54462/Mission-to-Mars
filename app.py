e80d7bd0d0889d47ea6fe6ab92b44e55fcd1ed8b
lkaruppiah54462 <lakshmazda@gmail.com>
2020-07-11 12:06:24 -0700

    flask app and update scrapping

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 from flask_pymongo import PyMongo
-import scraping
+import scrapping
 
 app = Flask(__name__)
 
@@ -16,7 +16,7 @@ def index():
 @app.route("/scrape")
 def scrape():
    mars = mongo.db.mars
-   mars_data = scraping.scrape_all()
+   mars_data = scrapping.scrape_all()
    mars.update({}, mars_data, upsert=True)
    return "Scraping Successful!"
 
